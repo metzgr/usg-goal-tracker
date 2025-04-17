b262c3f8afdf2d4a370f920d21cdbb10da43add8
Ivan Metzger <ivan.metzger@gsa.gov>
2025-04-17 11:15:05 -0400

    update

--- src/app/analyze/page.tsx
+++ src/app/analyze/page.tsx
@@ -4,8 +4,19 @@ import React, { useState } from "react";
 import UsgBanner from "src/components/custom/usg-banner";
 import Navigation from "src/components/custom/navigation";
 import Header from "src/components/custom/header";
+import metrics from "@/data/metric.json";
+import metricResults from "@/data/metricResult.json";
 
-export default function ExplorePage() {
+export default function AnalyzePage() {
+  const tableData = metrics.map((m) => ({ ...m, type: "metric" }));
+  const groupedMetricResults = metricResults.reduce((acc, result) => {
+    const metricId = result.metric?.[0];
+    if (metricId) {
+      if (!acc[metricId]) acc[metricId] = [];
+      acc[metricId].push(result);
+    }
+    return acc;
+  }, {});
 
   return (
     <div>
