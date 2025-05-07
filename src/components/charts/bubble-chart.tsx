3a3ab5a19313dad11c568337fb6a1d27a93e7212
Ivan Metzger <ivan.metzger@gsa.gov>
2025-05-07 12:47:05 -0400

    Update bubble-chart.tsx

--- src/components/charts/bubble-chart.tsx
+++ src/components/charts/bubble-chart.tsx
@@ -43,7 +43,7 @@ export default function BubbleChart({
       .append("rect")
       .attr("width", patternTileWidth)
       .attr("height", patternTileHeight)
-      .attr("fill", "#00359E");
+      .attr("fill", "#444CE7");
     
     const imgWidth = patternTileWidth - margin;
     const imgHeight = patternTileHeight - margin;
